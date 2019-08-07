c536410aec6db1599f7a080cd395f61f05b6ea22
kazuhei <kazuhei0108@gmail.com>
2019-08-07 15:02:40 +0900

    contentfulにつなぎこむ

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,17 @@
 const path = require('path');
 
+const pkg = require('./package')
+const { getConfigForKeys } = require('./lib/config.js')
+const ctfConfig = getConfigForKeys([
+  'CTF_BLOG_POST_TYPE_ID',
+  'CTF_SPACE_ID',
+  'CTF_CDA_ACCESS_TOKEN'
+])
+
+const { createClient } = require('./plugins/contentful')
+const cdaClient = createClient(ctfConfig)
+
+
 export default {
   mode: 'universal',
   /*
@@ -45,12 +57,18 @@ export default {
     extend(config, ctx) {
     },
   },
-  resolve: {
-    extensions: ['.js', '.json', '.vue', '.ts'],
-    root: path.resolve(__dirname),
-    alias: {
-      '@': path.resolve(__dirname),
-      '~': path.resolve(__dirname),
-    },
+  generate: {
+    routes() {
+      return cdaClient
+      .getEntries(ctfConfig.CTF_BLOG_POST_TYPE_ID)
+      .then(entries => {
+        return [...entries.items.map(entry => `/blog/${entry.fields.slug}`)]
+      })
+    }
   },
+  env: {
+    CTF_SPACE_ID: ctfConfig.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: ctfConfig.CTF_CDA_ACCESS_TOKEN,
+    CTF_BLOG_POST_TYPE_ID: ctfConfig.CTF_BLOG_POST_TYPE_ID
+  }
 };
